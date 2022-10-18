068a892d56be2deb561528679d70bdcd70271582
Ed Orsini <edorsini@gmail.com>
2022-10-18 19:50:10 -0400

    add adapter, decorator, facade, singleton, state patterns

---
+++ decorator/Main.java
@@ -0,0 +1,17 @@
+package patterns.decorator;
+
+public class Main {
+    public static void main(String[] args) {
+        Car sportyCar = new HondaAccord();
+        System.out.println(sportyCar.getDescription() + ", " + sportyCar.getCost());
+
+        sportyCar = new Sunroof(sportyCar);
+        System.out.println(sportyCar.getDescription() + ", " + sportyCar.getCost());
+
+        sportyCar = new Leather(sportyCar);
+        System.out.println(sportyCar.getDescription() + ", " + sportyCar.getCost());
+
+        sportyCar = new Foglight(sportyCar);
+        System.out.println(sportyCar.getDescription() + ", " + sportyCar.getCost());
+    }
+}
